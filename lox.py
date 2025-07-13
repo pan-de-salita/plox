643dc78a2bd75d393a437fd41b2c4c5b71f46e04
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2025-07-13 18:02:40 +0800

    create lox runners

--- lox.py
+++ lox.py
@@ -3,19 +3,36 @@ import sys
 
 class Lox:
     @staticmethod
-    def main(args: list[str]):
+    def main(args: list[str]) -> None:
         if len(args) > 1:
             print("Usage: jlox [script]")
+
+            # 64 is code for "command line usage error" in Unix/POSIX conventions
             sys.exit(64)
         elif len(args) == 1:
-            Lox.run_file(args[0])
+            Lox.fun_file(args[0])
         else:
             Lox.run_prompt()
 
     @staticmethod
-    def run_file(arg: str):
-        pass
+    def run_file(path: str) -> None:
+        """File runner"""
+        with open(path, encoding="utf-8") as bytes:
+            Lox.run(bytes)
 
     @staticmethod
-    def run_prompt():
-        pass
+    def run_prompt() -> None:
+        """Prompt"""
+        while True:
+            user_input = input("> ")
+            if not user_input:
+                break
+            Lox.run(user_input)
+
+    @staticmethod
+    def run(source: str) -> None:
+        scanner = Scanner(source)  # Scanner class not yet defined
+        tokens = scanner.scan_tokens()
+
+        for token in tokens:
+            print(token)
