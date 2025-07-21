0fdc8d5ee8713ee8a34b81cbddc649cd3c946ea2
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2025-07-21 23:11:59 +0800

    handle identifiers and add comments for methods

--- lox_scanner.py
+++ lox_scanner.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from types import MappingProxyType
 from typing import Any
 
 from lox_token import LoxToken
@@ -12,8 +13,34 @@ class LoxScanner:
     start: int = 0
     current: int = 0
     line: int = 1
+    keywords: MappingProxyType[str, LoxTokenType] = MappingProxyType(
+        {
+            keyword.name.lower(): keyword
+            for keyword in [
+                LoxTokenType.AND,
+                LoxTokenType.CLASS,
+                LoxTokenType.ELSE,
+                LoxTokenType.FALSE,
+                LoxTokenType.FOR,
+                LoxTokenType.FUN,
+                LoxTokenType.IF,
+                LoxTokenType.NIL,
+                LoxTokenType.OR,
+                LoxTokenType.PRINT,
+                LoxTokenType.RETURN,
+                LoxTokenType.SUPER,
+                LoxTokenType.THIS,
+                LoxTokenType.TRUE,
+                LoxTokenType.VAR,
+                LoxTokenType.WHILE,
+            ]
+        }
+    )
 
     def scan_tokens(self) -> list[LoxToken]:
+        """
+        Scans tokens from source.
+        """
         # For inspection purposes.
         print(repr(self.source))
 
@@ -25,6 +52,9 @@ class LoxScanner:
         return self.tokens
 
     def __scan_token(self) -> None:
+        """
+        Scan an individual token.
+        """
         char: str = self.__advance()
 
         match char:
@@ -83,25 +113,42 @@ class LoxScanner:
             case _:
                 if self.__is_digit(char):
                     self.__number()
+                elif self.__is_alpha(char):
+                    self.__identifier()
                 else:
                     from lox import Lox
 
                     Lox.error(self.line, "Unexpected character.")
 
     def __is_at_end(self) -> bool:
+        """
+        Checks if current points to the end of source. Useful for preventing
+        IndexError.
+        """
         return self.current >= len(self.source)
 
     def __advance(self) -> str:
+        """
+        Advances current.
+        Returns current scanned character in source.
+        """
         self.current += 1
         return self.source[self.current - 1]
 
     def __add_token(self, type: LoxTokenType, literal: Any = None) -> None:
+        """
+        Adds a token to self.tokens.
+        """
         # NOTE: self.current will be incremented to the right index because of
         # self.advance().
         text: str = self.source[self.start : self.current]
         self.tokens += [LoxToken(type, text, literal, self.line)]
 
     def __match(self, expected: str) -> bool:
+        """
+        Checks if expected matches the current character being scanned.
+        Advances current if True.
+        """
         if self.__is_at_end():
             return False
 
@@ -112,12 +159,19 @@ class LoxScanner:
         return True
 
     def __peek(self) -> str:
+        """
+        Looks ahead by one character in source.
+        """
         if self.__is_at_end():
             return "\0"
 
         return self.source[self.current]
 
     def __string(self) -> None:
+        """
+        Produces a string token.
+        """
+        # Use of self.__is_at_end() needed to avoid IndexError.
         while self.__peek() != '"' and not self.__is_at_end():
             if self.__peek() == "\n":
                 self.line += 1
@@ -136,8 +190,8 @@ class LoxScanner:
         string: str = self.source[self.start + 1 : self.current - 1]
         self.__add_token(LoxTokenType.STRING, string)
 
-    # Own version
-    # Less explicit about advancing to consume second quote
+    # Own version.
+    # Less explicit about advancing to consume second quote.
     # def __string(self):
     #     while not self.__match('"') and not self.__is_at_end():
     #         if self.__peek() == "\n":
@@ -154,10 +208,16 @@ class LoxScanner:
     #         LoxTokenType.STRING, self.source[self.start + 1 : self.current - 1]
     #     )
 
-    def __is_digit(self, c: str) -> bool:
-        return c >= "0" and c <= "9"
+    def __is_digit(self, char: str) -> bool:
+        """
+        Checks if char is a digit.
+        """
+        return char in map(str, range(0, 10))
 
     def __number(self) -> None:
+        """
+        Produces a number token.
+        """
         while self.__is_digit(self.__peek()):
             self.__advance()
 
@@ -172,7 +232,43 @@ class LoxScanner:
         )
 
     def __peek_next(self) -> str:
+        """
+        Looks ahead by two characters in source.
+        """
         if self.current + 1 >= len(self.source):
             return "\0"
 
         return self.source[self.current + 1]
+
+    def __identifier(self) -> None:
+        """
+        Produces an identifier token.
+
+        NOTE: An identifier can be:
+        - An identifier proper
+        - A reserved keyword
+        """
+        while self.__is_alpha_numeric(self.__peek()):
+            self.__advance()
+
+        identifier = self.source[self.start : self.current]
+        keyword = self.keywords.get(identifier)
+
+        if keyword:
+            self.__add_token(keyword)
+        else:
+            self.__add_token(LoxTokenType.IDENTIFIER)
+
+    def __is_alpha_numeric(self, char: str) -> bool:
+        """
+        Checks if char is alphanumeric.
+        """
+        return self.__is_alpha(char) or self.__is_digit(char)
+
+    def __is_alpha(self, char: str) -> bool:
+        """
+        Checks if char is alphabetical or an underscore.
+        """
+        import string
+
+        return char in string.ascii_letters + "_"
