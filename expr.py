5a7ac7d4193b03d6856381c97e4b0917acb494c5
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2025-07-26 23:44:10 +0800

    add notes on parsing expression grammar

--- expr.py
+++ expr.py
@@ -12,3 +12,26 @@
 #
 # operator -> "+" | "-" | "*" | "/"
 #           | "==" | "!=" | "<" | "<=" | ">" | ">=";
+
+# Expansion of 1 + 2 != 4
+#
+#         binary(!= )
+#        /           \
+#    binary(+)      lit(4)
+#    /   |   \
+# lit(1) "+" lit(2)
+#
+# 0. expression
+# 1. binary
+# 2. expression operator expression
+# 3. binary "!=" expression
+# 4. (expression operator expression) "!=" expression
+# 5. (literal "+" expression) "!=" expression
+# 6. (NUMBER "+" expression) "!=" expression
+# 7. (NUMBER "+" literal) "!=" expression
+# 8. (NUMBER "+" NUMBER) "!=" expression
+# 9. ("1" "+" NUMBER) "!=" expression
+# 10. ("1" "+" "2") "!=" expression
+# 11. ("1" "+" "2") "!=" literal
+# 12. ("1" "+" "2") "!=" NUMBER
+# 13. ("1" "+" "2") "!=" "4"
