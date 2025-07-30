304636e9c3ef4d63a0d6445b7da9344601ebf929
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2025-07-30 23:13:04 +0800

    complete a (not very) pretty printer

--- lox/ast_printer.py
+++ lox/ast_printer.py
@@ -3,19 +3,53 @@ from .token import Token
 from .token_type import TokenType
 
 
-class AstPrinter(expr.Visitor):
+class AstPrinter(expr.Visitor[str]):
     """A (not very) pretty printer."""
 
     @staticmethod
+    # NOTE: Should accept an Expr as argument.
     def main() -> None:
+        # (+ 1 2)
+        # expression: expr.Expr = expr.Binary(
+        #     left=expr.Literal(value=1),
+        #     operator=Token(type=TokenType.PLUS, lexeme="+", literal=None, line=1),
+        #     right=expr.Literal(value=2),
+        # )
+
         # (* (- 123) (group 45.67))
+        # expression: expr.Expr = expr.Binary(
+        #     left=expr.Unary(
+        #         operator=Token(type=TokenType.MINUS, lexeme="-", literal=None, line=1),
+        #         right=expr.Literal(value=123),
+        #     ),
+        #     operator=Token(type=TokenType.STAR, lexeme="*", literal=None, line=1),
+        #     right=expr.Grouping(expression=expr.Literal(value=45.67)),
+        # )
+
+        # (* (- 123) (* (- 0.4 (/ 5 6)) (group 78.91)))
         expression: expr.Expr = expr.Binary(
             left=expr.Unary(
                 operator=Token(type=TokenType.MINUS, lexeme="-", literal=None, line=1),
                 right=expr.Literal(value=123),
             ),
             operator=Token(type=TokenType.STAR, lexeme="*", literal=None, line=1),
-            right=expr.Grouping(expr.Literal(value=45.67)),
+            right=expr.Binary(
+                left=expr.Binary(
+                    operator=Token(
+                        type=TokenType.MINUS, lexeme="-", literal=None, line=1
+                    ),
+                    left=expr.Literal(value=0.4),
+                    right=expr.Binary(
+                        left=expr.Literal(value=5),
+                        operator=Token(
+                            type=TokenType.SLASH, lexeme="/", literal=None, line=1
+                        ),
+                        right=expr.Literal(value=6),
+                    ),
+                ),
+                operator=Token(type=TokenType.STAR, lexeme="*", literal=None, line=1),
+                right=expr.Grouping(expression=expr.Literal(value=78.910)),
+            ),
         )
 
         printer = AstPrinter()
@@ -25,13 +59,48 @@ class AstPrinter(expr.Visitor):
         return expr.accept(self)
 
     def visit_binary_expr(self, binary: expr.Binary) -> str:
-        return ""
+        return self.__parenthesize(binary.operator.lexeme, binary.left, binary.right)
 
     def visit_grouping_expr(self, grouping: expr.Grouping) -> str:
-        return ""
+        return self.__parenthesize("group", grouping.expression)
 
     def visit_literal_expr(self, literal: expr.Literal) -> str:
-        return ""
+        return self.__parenthesize(str(literal.value))
 
     def visit_unary_expr(self, unary: expr.Unary) -> str:
-        return ""
+        return self.__parenthesize(unary.operator.lexeme, unary.right)
+
+    def __parenthesize(self, name: str, *exprs: expr.Expr) -> str:
+        # Iter 1 (Funtional):
+        # from functools import reduce
+        #
+        # return (
+        #     ("(" if exprs else "")
+        #     + name
+        #     + reduce(lambda acc, curr: acc + " " + (curr.accept(self)), exprs, "")
+        #     + (")" if exprs else "")
+        # )
+
+        # Iter 2 (Imperative):
+        # parenthasized = name
+        # for e in exprs:
+        #     parenthasized += f" {e.accept(self)}"
+        #
+        # return parenthasized if not exprs else f"({parenthasized})"
+
+        # Iter 3:
+        # return (
+        #     ("(" if exprs else "")
+        #     + name
+        #     + "".join(" " + e.accept(self) for e in exprs)
+        #     + (")" if exprs else "")
+        # )
+
+        if not exprs:
+            return name
+
+        return f"({name}{''.join(' ' + e.accept(self) for e in exprs)})"
+
+
+if __name__ == "__main__":
+    AstPrinter.main()
