75a17fbcecdd3d4b1c21a5745546187bb730ebbf
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2025-07-30 23:19:28 +0800

    add doc strings; improve main

--- lox/ast_printer.py
+++ lox/ast_printer.py
@@ -7,70 +7,80 @@ class AstPrinter(expr.Visitor[str]):
     """A (not very) pretty printer."""
 
     @staticmethod
-    # NOTE: Should accept an Expr as argument.
-    def main() -> None:
-        # (+ 1 2)
-        # expression: expr.Expr = expr.Binary(
-        #     left=expr.Literal(value=1),
-        #     operator=Token(type=TokenType.PLUS, lexeme="+", literal=None, line=1),
-        #     right=expr.Literal(value=2),
-        # )
+    def main(expression: expr.Expr | None = None) -> None:
+        if not expression:
+            # (+ 1 2)
+            # expression: expr.Expr = expr.Binary(
+            #     left=expr.Literal(value=1),
+            #     operator=Token(type=TokenType.PLUS, lexeme="+", literal=None, line=1),
+            #     right=expr.Literal(value=2),
+            # )
 
-        # (* (- 123) (group 45.67))
-        # expression: expr.Expr = expr.Binary(
-        #     left=expr.Unary(
-        #         operator=Token(type=TokenType.MINUS, lexeme="-", literal=None, line=1),
-        #         right=expr.Literal(value=123),
-        #     ),
-        #     operator=Token(type=TokenType.STAR, lexeme="*", literal=None, line=1),
-        #     right=expr.Grouping(expression=expr.Literal(value=45.67)),
-        # )
+            # (* (- 123) (group 45.67))
+            # expression: expr.Expr = expr.Binary(
+            #     left=expr.Unary(
+            #         operator=Token(type=TokenType.MINUS, lexeme="-", literal=None, line=1),
+            #         right=expr.Literal(value=123),
+            #     ),
+            #     operator=Token(type=TokenType.STAR, lexeme="*", literal=None, line=1),
+            #     right=expr.Grouping(expression=expr.Literal(value=45.67)),
+            # )
 
-        # (* (- 123) (* (- 0.4 (/ 5 6)) (group 78.91)))
-        expression: expr.Expr = expr.Binary(
-            left=expr.Unary(
-                operator=Token(type=TokenType.MINUS, lexeme="-", literal=None, line=1),
-                right=expr.Literal(value=123),
-            ),
-            operator=Token(type=TokenType.STAR, lexeme="*", literal=None, line=1),
-            right=expr.Binary(
-                left=expr.Binary(
+            # (* (- 123) (* (- 0.4 (/ 5 6)) (group 78.91)))
+            expression = expr.Binary(
+                left=expr.Unary(
                     operator=Token(
                         type=TokenType.MINUS, lexeme="-", literal=None, line=1
                     ),
-                    left=expr.Literal(value=0.4),
-                    right=expr.Binary(
-                        left=expr.Literal(value=5),
-                        operator=Token(
-                            type=TokenType.SLASH, lexeme="/", literal=None, line=1
-                        ),
-                        right=expr.Literal(value=6),
-                    ),
+                    right=expr.Literal(value=123),
                 ),
                 operator=Token(type=TokenType.STAR, lexeme="*", literal=None, line=1),
-                right=expr.Grouping(expression=expr.Literal(value=78.910)),
-            ),
-        )
+                right=expr.Binary(
+                    left=expr.Binary(
+                        operator=Token(
+                            type=TokenType.MINUS, lexeme="-", literal=None, line=1
+                        ),
+                        left=expr.Literal(value=0.4),
+                        right=expr.Binary(
+                            left=expr.Literal(value=5),
+                            operator=Token(
+                                type=TokenType.SLASH, lexeme="/", literal=None, line=1
+                            ),
+                            right=expr.Literal(value=6),
+                        ),
+                    ),
+                    operator=Token(
+                        type=TokenType.STAR, lexeme="*", literal=None, line=1
+                    ),
+                    right=expr.Grouping(expression=expr.Literal(value=78.910)),
+                ),
+            )
 
         printer = AstPrinter()
         print(printer.print(expression))
 
     def print(self, expr: expr.Expr) -> str:
+        """Pretty-print an expression in Lisp format."""
         return expr.accept(self)
 
     def visit_binary_expr(self, binary: expr.Binary) -> str:
+        """Overrides Visitor.visit_binary_expr()."""
         return self.__parenthesize(binary.operator.lexeme, binary.left, binary.right)
 
     def visit_grouping_expr(self, grouping: expr.Grouping) -> str:
+        """Overrides Visitor.visit_grouping_expr()."""
         return self.__parenthesize("group", grouping.expression)
 
     def visit_literal_expr(self, literal: expr.Literal) -> str:
+        """Overrides Visitor.visit_literal_expr()."""
         return self.__parenthesize(str(literal.value))
 
     def visit_unary_expr(self, unary: expr.Unary) -> str:
+        """Overrides Visitor.visit_unary_expr()."""
         return self.__parenthesize(unary.operator.lexeme, unary.right)
 
     def __parenthesize(self, name: str, *exprs: expr.Expr) -> str:
+        """Parenthasize expression unless exprs in None."""
         # Iter 1 (Funtional):
         # from functools import reduce
         #
