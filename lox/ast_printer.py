619b2d048a101febff67541993ef38768a5d5620
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2025-07-30 21:41:15 +0800

    complete ast_printer boilerplate

--- lox/ast_printer.py
+++ lox/ast_printer.py
@@ -1,8 +1,37 @@
-import
+from . import expr
+from .token import Token
+from .token_type import TokenType
 
-class AstPrinter:
+
+class AstPrinter(expr.Visitor):
     """A (not very) pretty printer."""
 
     @staticmethod
     def main() -> None:
         # (* (- 123) (group 45.67))
+        expression: expr.Expr = expr.Binary(
+            left=expr.Unary(
+                operator=Token(type=TokenType.MINUS, lexeme="-", literal=None, line=1),
+                right=expr.Literal(value=123),
+            ),
+            operator=Token(type=TokenType.STAR, lexeme="*", literal=None, line=1),
+            right=expr.Grouping(expr.Literal(value=45.67)),
+        )
+
+        printer = AstPrinter()
+        print(printer.print(expression))
+
+    def print(self, expr: expr.Expr) -> str:
+        return expr.accept(self)
+
+    def visit_binary_expr(self, binary: expr.Binary) -> str:
+        return ""
+
+    def visit_grouping_expr(self, grouping: expr.Grouping) -> str:
+        return ""
+
+    def visit_literal_expr(self, literal: expr.Literal) -> str:
+        return ""
+
+    def visit_unary_expr(self, unary: expr.Unary) -> str:
+        return ""
