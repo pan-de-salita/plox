35834b770fe0af4ed351679a7d5aa0fbb103907c
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2025-09-17 20:57:35 +0800

    chore: add enclosing attr to Environment

--- lox/environment.py
+++ lox/environment.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from typing import Self
 
 from .runtime_exception import RuntimeException
 from .token import Token
@@ -7,24 +8,33 @@ from .token import Token
 @dataclass()
 class Environment:
     _values: dict[str, object] = field(default_factory=dict)
+    enclosing: Self | None = None
 
     def define(self, name_lexeme: str, value: object) -> None:
+        """Define a variable."""
         self._values[name_lexeme] = value
 
     def get(self, name: Token) -> object:
         """Gets a variable's value if it's name exists in the Environment's
         values, else raise a RuntimeException."""
-        try:
+        if name in self._values.keys():
             return self._values[name.lexeme]
-        except Exception:
-            raise RuntimeException(
-                token=name, message=f"Undefined variable: {name.lexeme}."
-            )
+
+        if self.enclosing:
+            return self.enclosing.get(name=name)
+
+        raise RuntimeException(
+            token=name, message=f"Undefined variable: {name.lexeme}."
+        )
 
     def assign(self, name: Token, value: object) -> None:
+        """Assign a value to a name."""
         if name.lexeme in self._values.keys():
             self._values[name.lexeme] = value
-        else:
-            raise RuntimeException(
-                token=name, message=f"Undefined variable: {name.lexeme}."
-            )
+
+        if self.enclosing:
+            return self.enclosing.assign(name=name, value=value)
+
+        raise RuntimeException(
+            token=name, message=f"Undefined variable: {name.lexeme}."
+        )
